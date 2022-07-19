234929031d2187d2fada7d9b8bc72f9d58af7909
Jed <vjeudy@protonmail.com>
2022-07-19 20:27:55 +0200

    uses eslint_d

--- nvim/.config/nvim/lua/user/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/user/lsp/null-ls.lua
@@ -11,10 +11,10 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup {
   debug = false,
   sources = {
-    formatting.prettier.with { extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } },
-    formatting.black.with { extra_args = { "--fast" } },
-    -- formatting.yapf,
     formatting.stylua,
-    diagnostics.flake8,
+    formatting.eslint_d,
+    diagnostics.eslint_d,
+    -- formatting.prettier,
+    -- diagnostics.flake8,
   },
 }
