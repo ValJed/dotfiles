7d94203de1e95eb9a55db060ab0399c900f3d289
Jed <vjeudy@protonmail.com>
2022-08-02 15:34:23 +0200

    udpates

--- nvim/.config/nvim/lua/user/lsp/configs.lua
+++ nvim/.config/nvim/lua/user/lsp/configs.lua
@@ -18,8 +18,10 @@ for _, server in pairs(servers) do
     capabilities = require("user.lsp.handlers").capabilities,
   }
   local has_custom_opts, server_custom_opts = pcall(require, "user.lsp.settings." .. server)
+
   if has_custom_opts then
     opts = vim.tbl_deep_extend("force", opts, server_custom_opts)
   end
+
   lspconfig[server].setup(opts)
 end
