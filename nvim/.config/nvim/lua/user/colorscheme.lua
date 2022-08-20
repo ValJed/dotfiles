d216192e7246e9359dc2f4aa1badffa1836a7d91
Jed <vjeudy@protonmail.com>
2022-08-20 12:37:06 +0200

    changes colorscheme

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,11 +1,10 @@
-vim.cmd "colorscheme default"
+vim.cmd("colorscheme default")
 
-local colorscheme = "nightfox"
+local colorscheme = "carbonfox"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 
 if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
-  return
+	vim.notify("colorscheme " .. colorscheme .. " not found!")
+	return
 end
-    
