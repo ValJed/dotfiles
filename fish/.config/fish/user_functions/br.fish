41ef3bb67021984ff2143b7109819c085ef3ea55
Jed <vjeudy@protonmail.com>
2022-09-14 17:25:45 -0500

    wip bright screens script

--- fish/.config/fish/user_functions/br.fish
+++ fish/.config/fish/user_functions/br.fish
@@ -1,4 +1,18 @@
 function br
-  set --local screens xrandr -q | grep " connected"
-  echo $screens
+  set --local brightness $argv[1]
+
+  # if $brightness == ""
+  #   echo "Set a brightness value"
+  #   return 1
+  # end
+
+  set --local connected_screens (command xrandr -q | grep ' connected')
+
+  for conn_screen in $connected_screens
+    set --local screen (command echo $conn_screen | cut -d ' ' -f1)
+
+    command xrandr --output
+    echo $screen
+  end
+
 end
