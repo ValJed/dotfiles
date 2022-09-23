74c3b02d49e1e7f96993d06d25e7875702595430
Jed <vjeudy@protonmail.com>
2022-09-23 18:23:24 -0500

    brightness script working

--- fish/.config/fish/user_functions/br.fish
+++ fish/.config/fish/user_functions/br.fish
@@ -1,18 +1,17 @@
 function br
   set --local brightness $argv[1]
 
-  # if $brightness == ""
-  #   echo "Set a brightness value"
-  #   return 1
-  # end
+  if [ -z  "$brightness"]
+    echo "Set a brightness value"
+    return 1
+  end
 
   set --local connected_screens (command xrandr -q | grep ' connected')
 
   for conn_screen in $connected_screens
     set --local screen (command echo $conn_screen | cut -d ' ' -f1)
-
-    command xrandr --output
-    echo $screen
+    
+    command xrandr --output $screen --brightness $brightness
   end
 
 end
