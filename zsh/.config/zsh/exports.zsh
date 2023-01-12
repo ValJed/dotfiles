84b291e7506917215dd5f739fee23200a4b2e12f
Jed <vjeudy@protonmail.com>
2023-01-12 19:24:25 +0100

    zsh reorga

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -10,8 +10,11 @@ export XDG_CURRENT_DESKTOP="i3"
 export PATH=$HOME/.local/bin:$PATH
 export PATH=$HOME/.cargo/bin:$PATH
 export PATH=$HOME/stagecoach/bin:$PATH
+export PATH="/opt/ops-tools/bin:$PATH"
 
-# #export PATH="$PATH:./node_modules/.bin"
-# eval "$(fnm env)"
-# eval "$(zoxide init zsh)"
-# eval "`pip completion --zsh`"
+export PNPM_HOME="/home/jed/.local/share/pnpm"
+export PATH="$PNPM_HOME:$PATH"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -22,11 +22,3 @@ plug "zsh-users/zsh-syntax-highlighting"
 # keybinds
 bindkey '^ ' autosuggest-accept
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-# pnpm
-export PNPM_HOME="/home/jed/.local/share/pnpm"
-export PATH="$PNPM_HOME:$PATH"
-# pnpm end
