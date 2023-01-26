a6ab1c09538a1dc76156605c0838753dca480659
Jed <vjeudy@protonmail.com>
2023-01-26 22:03:18 +0100

    nvim: adds lualine

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -61,6 +61,11 @@ return packer.startup(function(use)
 		"akinsho/toggleterm.nvim",
 		tag = "*",
 	})
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
+
 	-- Color scheme
 	use("EdenEast/nightfox.nvim")
 
