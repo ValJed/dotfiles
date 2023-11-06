7e03e38d89eb213366a7639bac6404101e585893
Jed <vjeudy@protonmail.com>
2023-11-06 22:51:28 +0100

    adds function to grep from nvim tree in the selected folder or parent one if a file

--- nvim/.config/nvim/lua/user/tree.lua
+++ nvim/.config/nvim/lua/user/tree.lua
@@ -3,6 +3,8 @@ if not status_ok then
 	return
 end
 
+local status_telescope, telescope = pcall(require, "telescope.builtin")
+
 local function on_attach(bufnr)
 	local api = require("nvim-tree.api")
 
@@ -16,11 +18,35 @@ local function on_attach(bufnr)
 		}
 	end
 
+	local live_grep_folder = function()
+		if not status_telescope then
+			return
+		end
+
+		local node = api.tree.get_node_under_cursor()
+		if not node or not node.absolute_path then
+			telescope.live_grep()
+			return
+		end
+
+		local is_directoy = vim.fn.isdirectory(node.absolute_path)
+		if is_directoy == 1 then
+			telescope.live_grep({
+				cwd = node.absolute_path,
+			})
+		else
+			telescope.live_grep({
+				cwd = vim.fn.fnamemodify(node.absolute_path, ":h"),
+			})
+		end
+	end
+
+	vim.keymap.set("n", "g", live_grep_folder, opts("Grep in Folder"))
+
 	vim.keymap.set("n", "l", api.node.open.edit, opts("Edit"))
 	vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Node"))
 	vim.keymap.set("n", "v", api.node.open.vertical, opts("Vertical Split"))
 	vim.keymap.set("n", "d", api.fs.remove, opts("Delete"))
-
 	vim.keymap.set("n", "<C-]>", api.tree.change_root_to_node, opts("CD"))
 	vim.keymap.set("n", "<C-e>", api.node.open.replace_tree_buffer, opts("Open: In Place"))
 	vim.keymap.set("n", "<C-k>", api.node.show_info_popup, opts("Info"))
@@ -50,8 +76,7 @@ local function on_attach(bufnr)
 	vim.keymap.set("n", "[e", api.node.navigate.diagnostics.prev, opts("Prev Diagnostic"))
 	vim.keymap.set("n", "F", api.live_filter.clear, opts("Clean Filter"))
 	vim.keymap.set("n", "f", api.live_filter.start, opts("Filter"))
-	vim.keymap.set("n", "g?", api.tree.toggle_help, opts("Help"))
-	vim.keymap.set("n", "gy", api.fs.copy.absolute_path, opts("Copy Absolute Path"))
+	vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
 	vim.keymap.set("n", "H", api.tree.toggle_hidden_filter, opts("Toggle Dotfiles"))
 	vim.keymap.set("n", "I", api.tree.toggle_gitignore_filter, opts("Toggle Git Ignore"))
 	vim.keymap.set("n", "J", api.node.navigate.sibling.last, opts("Last Sibling"))
@@ -70,6 +95,7 @@ local function on_attach(bufnr)
 	vim.keymap.set("n", "W", api.tree.collapse_all, opts("Collapse"))
 	vim.keymap.set("n", "x", api.fs.cut, opts("Cut"))
 	vim.keymap.set("n", "y", api.fs.copy.filename, opts("Copy Name"))
+	--[[ vim.keymap.set("n", "Y", api.fs.copy.absolute_path, opts("Copy Absolute Path")) ]]
 	vim.keymap.set("n", "Y", api.fs.copy.relative_path, opts("Copy Relative Path"))
 	vim.keymap.set("n", "<2-LeftMouse>", api.node.open.edit, opts("Open"))
 	vim.keymap.set("n", "<2-RightMouse>", api.tree.change_root_to_node, opts("CD"))
