5ba9869d8ab8858fbb40d2f67ef01fcbb036b7c3
Jed <vjeudy@protonmail.com>
2024-03-25 23:09:42 +0100

    adds dadbod completion

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -79,7 +79,7 @@ local plugins = {
 		"kristijanhusak/vim-dadbod-ui",
 		dependencies = {
 			{ "tpope/vim-dadbod", lazy = true },
-			--[[ { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, ]]
+			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "plsql" }, lazy = true },
 		},
 		cmd = {
 			"DBUI",
