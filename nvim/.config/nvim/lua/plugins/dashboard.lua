6aa822764bc7ad0029067b062b210eddb2c6ec4c
Jed <vjeudy@protonmail.com>
2024-04-11 10:41:54 +0200

    updates dashboard icons

--- nvim/.config/nvim/lua/plugins/dashboard.lua
+++ nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -35,12 +35,12 @@ return {
 			}
 
 			dashboard.section.buttons.val = {
-				dashboard.button("e", "  New File", ":enew<cr>"),
-				dashboard.button("f", "  Find File", ":Telescope find_files hidden=true<cr>"),
+				dashboard.button("e", "  New File", ":enew<cr>"),
+				dashboard.button("f", "󰮗  Find File", ":Telescope find_files hidden=true<cr>"),
 				dashboard.button("r", "  Recent File", ":Telescope oldfiles<cr>"),
-				dashboard.button("t", "  Find Text", ":Telescope live_grep<cr>"),
-				dashboard.button("p", "  Find Project", ":Telescope workspaces<cr>"),
-				dashboard.button("q", "  Quit", ":qa<cr>"),
+				dashboard.button("t", "󱘢  Find Text", ":Telescope live_grep<cr>"),
+				dashboard.button("p", "󰥨  Find Project", ":Telescope workspaces<cr>"),
+				dashboard.button("q", "󰜺  Quit", ":qa<cr>"),
 			}
 
 			dashboard.section.footer.val = {
