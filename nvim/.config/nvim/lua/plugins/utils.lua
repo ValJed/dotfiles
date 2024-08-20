4669d44caa0b62f66aa4c85df27e7df4c7adb760
Jed <vjeudy@protonmail.com>
2024-08-20 09:41:46 +0200

    adds flash

--- nvim/.config/nvim/lua/plugins/utils.lua
+++ nvim/.config/nvim/lua/plugins/utils.lua
@@ -1,3 +1,12 @@
+function Opts(desc)
+	local opt = { noremap = true, silent = true }
+	if desc then
+		opt.desc = desc
+	end
+
+	return opt
+end
+
 return {
 	{
 		"numToStr/Navigator.nvim",
@@ -12,6 +21,32 @@ return {
 		tag = "stable",
 		config = function()
 			require("crates").setup()
+		end,
+	},
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		config = function()
+			local flash = require("flash")
+			flash.setup()
+
+			--[[ local keymap = vim.api.nvim_set_keymap ]]
+			--[[]]
+			--[[ keymap({ "n", "x", "o" }, "s", function() ]]
+			--[[ 	flash.jump() ]]
+			--[[ end, Opts("Flash")) ]]
+			--[[ keymap({ "n", "x", "o" }, "S", function() ]]
+			--[[ 	flash.treesitter() ]]
+			--[[ end, Opts("Flash Treesitter")) ]]
+			--[[ keymap("o", "r", function() ]]
+			--[[ 	flash.remote() ]]
+			--[[ end, Opts("Remote Flash")) ]]
+			--[[ keymap({ "o", "x" }, "R", function() ]]
+			--[[ 	flash.treesitter_search() ]]
+			--[[ end, Opts("Treesitter Search")) ]]
+			--[[ keymap("c", "<c-s>", function() ]]
+			--[[ 	flash.toggle() ]]
+			--[[ end, opts("toggle flash search")) ]]
 		end,
 	},
 }
