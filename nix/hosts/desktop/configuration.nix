d7d80baf0a935ab26553b7d77b249bc41c1cd0e7
Jed <vjeudy@protonmail.com>
2024-11-13 00:15:27 +0100

    adds steam to desktop

--- nix/hosts/desktop/configuration.nix
+++ nix/hosts/desktop/configuration.nix
@@ -1,10 +1,17 @@
-{ pkgs, lib, inputs, hostname, ... }:
+{ pkgs, inputs, hostname, ... }:
 
 {
   imports = [
     ./hardware-configuration.nix
     inputs.self.outputs.nixosModules.default
   ];
+
+  programs.steam = {
+    enable = true;
+    # remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    # dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    # localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
 
   services.xserver.videoDrivers = ["nvidia"];
 
