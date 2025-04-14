593d6121fdc9c341cb1b5e5f3b682869b6afe16d
Jed <vjeudy@protonmail.com>
2025-04-14 11:30:59 +0200

    removes print

--- nvim/lua/lsp/settings/ts_ls.lua
+++ nvim/lua/lsp/settings/ts_ls.lua
@@ -2,7 +2,6 @@ local util = require("lspconfig.util")
 local vue_ts_plugin = os.getenv("VOLAR_LS")
 local root_dir = util.root_pattern("package.json")
 
-print(vim.inspect(vue_ts_plugin))
 return {
 	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
 	root_dir = root_dir,
