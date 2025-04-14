dd2a112a78c3dc4a7d98e8d3201dc2ba4a2b2ed4
Jed <vjeudy@protonmail.com>
2025-04-14 11:28:39 +0200

    updates ts lsp for vue

--- nvim/lua/lsp/settings/ts_ls.lua
+++ nvim/lua/lsp/settings/ts_ls.lua
@@ -1,7 +1,21 @@
+local util = require("lspconfig.util")
+local vue_ts_plugin = os.getenv("VOLAR_LS")
+local root_dir = util.root_pattern("package.json")
+
+print(vim.inspect(vue_ts_plugin))
 return {
+	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+	root_dir = root_dir,
 	init_options = {
 		preferences = {
 			disableSuggestions = true,
+		},
+		plugins = {
+			{
+				name = "@vue/typescript-plugin",
+				location = vue_ts_plugin,
+				languages = { "vue" },
+			},
 		},
 	},
 }
