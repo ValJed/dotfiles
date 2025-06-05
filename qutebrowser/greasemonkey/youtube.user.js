23fec4eea6bfb17ed9dc04556d45a498ea27ab59
Jed <vjeudy@protonmail.com>
2025-06-05 14:46:27 +0200

    qutebrowser stuff

--- qutebrowser/greasemonkey/youtube.user.js
+++ qutebrowser/greasemonkey/youtube.user.js
@@ -7,13 +7,27 @@
 // ==/UserScript==
 
 
-document.addEventListener('load', () => {
-    const ignoreBtn = document.querySelector('button.ytp-skip-ad-button')
-    if (ignoreBtn) {
-      ignoreBtn.click()
-    }
-    const ad = [...document.querySelectorAll('.ad-showing')][0];
-    if (ad) {
-        document.querySelector('video').currentTime = 9999999999;
-    }
+document.addEventListener('DOMContentLoaded', () => {
+  const ytHost = 'www.youtube.com'
+
+  console.log('window.location.hostname',window.location.hostname )
+  if (window.location.hostname === ytHost) {
+    const body = document.querySelector('body');
+    body.setAttribute('style', 'height: 100vh; width: 100vw; display: flex; flex-direction: column; justify-content: center; align-items: center;');
+    const pageContent = document.createElement('div');
+    const title = document.createElement('h1');
+    title.textContent = 'Go back to work!';
+    pageContent.appendChild(title);
+    body.innerHTML = '';
+    body.appendChild(pageContent);
+  }
+
+  /* const ignoreBtn = document.querySelector('button.ytp-skip-ad-button') */
+  /* if (ignoreBtn) { */
+  /*   ignoreBtn.click() */
+  /* } */
+  /* const ad = [...document.querySelectorAll('.ad-showing')][0]; */
+  /* if (ad) { */
+  /*     document.querySelector('video').currentTime = 9999999999; */
+  /* } */
 }, true);
