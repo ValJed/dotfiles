a37b7a9c4754f0fc5aec1c736c55bf3b68615506
Jed <vjeudy@protonmail.com>
2025-07-14 09:23:33 +0200

    rollback to sddm

--- nix/nixosModules/services/displayManager.nix
+++ nix/nixosModules/services/displayManager.nix
@@ -1,8 +1,26 @@
-{...}: {
-  services.displayManager.ly = {
+{pkgs, ...}: {
+  # Not working
+  # services.displayManager.ly = {
+  #   enable = true;
+  #   settings = {
+  #     animation = "matrix";
+  #   };
+  # };
+
+  environment.systemPackages = [
+    (
+      pkgs.catppuccin-sddm.override {
+        flavor = "mocha";
+        font = "FiraCode Nerd Font";
+        fontSize = "12";
+        background = "${../../../assets/wallpaper.png}";
+        loginBackground = true;
+      }
+    )
+  ];
+  services.displayManager.sddm = {
     enable = true;
-    settings = {
-      animation = "matrix";
-    };
+    theme = "catppuccin-mocha";
+    package = pkgs.kdePackages.sddm;
   };
 }
