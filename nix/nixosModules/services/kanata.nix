6e6ee21387f03a4965a1626f43748662c5ae5594
Jed <vjeudy@protonmail.com>
2025-07-21 22:55:21 +0200

    kanata switch to chords instead of tap hold

--- nix/nixosModules/services/kanata.nix
+++ nix/nixosModules/services/kanata.nix
@@ -26,7 +26,7 @@
             chord-m 50
           )
           (defalias
-            caps (tap-hold $tap-time $hold-time esc lctl)
+            caps esc
             d_shft (tap-hold $tap-time $hold-time d lsft)
             k_shft (tap-hold $tap-time $hold-time k lsft)
             f_meta (tap-hold $tap-time $hold-time f lmeta)
@@ -44,22 +44,35 @@
           (defchordsv2
             ;; Left Hand
             (z x c) @game_toggle $chord-s all-released ()
+            (d f) lctrl $chord-s all-released (game)
+            (s d) lshft $chord-s all-released (game)
+            (s d f) (multi lshft lctrl) $chord-s all-released (game)
+            (a s) ralt $chord-s all-released (game)
+            (w e) lmeta $chord-s all-released (game)
+            (w e r) (multi lmeta rshft) $chord-s all-released (game)
+
+            ;; Right Hand
+            (j k) lctrl $chord-s all-released (game)
+            (k l) lshft $chord-s all-released (game)
+            (j k l) (multi lshft lctrl) $chord-s all-released (game)
+            (l ;) ralt $chord-s all-released (game)
+            (i o) lmeta $chord-s all-released ()
+            (u i o) (multi lmeta rshft) $chord-s all-released ()
           )
 
           (deflayer base
             grv   1    2      3    4    5    6    7    8    9    0    -    =    bspc
             tab   q    w      e    r    t    y    u    i    o    p    [    ]    ret
-            @caps a    @s_alt @d_shft   @f_meta   g    h    @j_meta    @k_shft  @l_alt  ;  '  \
+            @caps a    s      d    f    g    h    j    k    l  ;  '  \
             lsft  z    x      c    v    b    n    m    ,    .    /    rsft
             lctl  lmet lalt             @spc_mod            ralt rmet rctl
           )
 
-          ;; Test to remove the S to brackets and stuff to take less space, could still be used using space + shift
           (deflayer special
-            _        f1   f2   f3   f4   f5   f6   f7    f8   f9   f10  f11   f12  _
-            S-prtsc  1    2    3    4    5    6    7     8    9    0    =     S-=    ret
-            caps     S-1  S-2  S-3  S-4  S-5  S-6  S-9   S-0  S-[  S-]  [     ]
-            C-S-prtsc   prev pp   next mute voldwn    volu  -    S--  S-7  S-8   rsft
+            _          f1   f2   f3   f4   f5   f6   f7    f8   f9   f10  f11   f12  _
+            S-prtsc    1    2    3    4    5    6    7     8    9    0    =     S-=    ret
+            caps       S-1  S-2  S-3  S-4  S-5  AG-6 S-9   S-0  [    ]  S-7     S-8
+            C-S-prtsc  prev pp   next mute voldwn    volu  -    S--  S-7  S-8   rsft
             prtsc      lmet lalt           @spc_mod        ralt rmet      rctl
           )
 
