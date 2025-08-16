e7a7006b90ad342d260e6e1bb19e5da15f00352e
Jed <vjeudy@protonmail.com>
2025-08-16 14:08:32 +0200

    updates kanata config

--- nix/nixosModules/services/kanata.nix
+++ nix/nixosModules/services/kanata.nix
@@ -43,10 +43,10 @@
               d_shft (tap-hold $tap-time $hold-time d lsft)
               k_shft (tap-hold $tap-time $hold-time k lsft)
               f_meta (tap-hold $tap-time $hold-time f lmeta)
-              j_meta (tap-hold $tap-time $hold-time j lmeta)
               s_alt (tap-hold $tap-time $hold-time s ralt)
               l_alt (tap-hold $tap-time $hold-time l ralt)
 
+              z_alt (tap-hold $tap-time $hold-time z lalt)
               /_alt (tap-hold $tap-time $hold-time / ralt)
 
               ;; Layouts
@@ -59,30 +59,26 @@
 
             (defchordsv2
               ;; Left Hand
-              (z x c) @game_toggle $chord-s all-released ()
+              (x c v) @game_toggle $chord-s all-released ()
 
               (d f) lctrl $chord-s all-released (game)
               (s d) lshft $chord-s all-released (game)
               (s d f) (multi lshft lctrl) $chord-s all-released (game)
               (a s) ralt $chord-s all-released (game)
-              (w e) lmeta $chord-s all-released (game)
-              (w e r) (multi lmeta rshft) $chord-s all-released (game)
 
               ;; Right Hand
               (j k) rctrl $chord-s all-released (game)
               (k l) rshft $chord-s all-released (game)
               (j k l) (multi lshft lctrl) $chord-s all-released (game)
               (l ;) ralt $chord-s all-released (game)
-              (i o) lmeta $chord-s all-released ()
-              (u i o) (multi lmeta rshft) $chord-s all-released ()
             )
 
             (deflayer base
-              grv   1    2      3    4    5    6    7    8    9     0       -    =    bspc    ;; 14
-              tab   q    w      e    r    t    y    u    i    o     p       \    ]    ret     ;; 14
-              @caps_mod  a      s    d    f    g    h    j    k     l  ;  '       bspc    ;; 13
-              lsft  z    x      c    v    b    n    m    ,    .     @/_alt  rsft    ;; 12
-              lctl  lmet lalt             @spc_mod            ralt  rmet    rctl    ;; 7
+              grv   1      2     3    4    5    6    7    8    9     0       -    =    bspc    ;; 14
+              tab   q      w     e    r    t    y    u    i    o     p       \    ]    ret     ;; 14
+              @caps_mod    a     s    d    f    g    h    j    k     l  ;  '       bspc    ;; 13
+              lsft  @z_alt x     c    v    b    n    m    ,    .     @/_alt  rsft    ;; 12
+              lctl  lalt   lmet             @spc_mod            ralt  rmet    rctl    ;; 7
             )
 
             (deflayer special
@@ -90,13 +86,13 @@
               S-prtsc    1    2    3    4    5    6     7     8    9    0    =     S-=    ret
               caps       S-1  S-2  S-3  S-4  S-5  AG-6  S-9   S-0  S-7  S-8  S-7   bspc
               C-S-prtsc  S-`  S-=  =    [    ]    AG-`  S-[   S-]  -    S--        rsft
-              _          lmet lalt           @spc_mod         ralt rmet            rctl
+              _          _    _         @spc_mod         ralt rmet            rctl
             )
 
             (deflayer media
               _          _          _     bldn       blup    _      _      _      _      _      _       _    _   _
-              _          prtsc      _     _          pp      _      home   pgdn   pgup   end    C-S-v   _    _   ret
-              _          S-prtsc    mute  voldwn     volu    _      left   down   up     rght   _       _    _
+              _          prtsc      _     mute       pp      _      home   pgdn   pgup   end    C-S-v   _    _   ret
+              _          S-prtsc    C-b   voldwn     volu    _      left   down   up     rght   _       _    _
               _          C-S-prtsc  _     next prev  next    volu   _      S--    S-7    S-8       rsft
               _          lmet lalt        @spc_mod           ralt   rmet          rctl
             )
