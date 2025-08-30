c343a8dfa502cfe97b145cbed509d69e174f2e6d
Jed <vjeudy@protonmail.com>
2025-08-30 11:37:56 +0200

    removes comments in home manager config

--- nix/homeManagerModules/default.nix
+++ nix/homeManagerModules/default.nix
@@ -38,15 +38,6 @@ in {
   home.username = "jed";
   home.homeDirectory = "/home/jed";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
-
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
@@ -156,22 +147,6 @@ in {
       config.lib.file.mkOutOfStoreSymlink "/home/jed/dotfiles/lazysql/config.toml";
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/jed/etc/profile.d/hm-session-vars.sh
-
   home.sessionVariables = {
     EDITOR = "nvim";
     ZSH_TMUX_CONFIG = "$HOME/.config/tmux/tmux.conf";
@@ -179,6 +154,14 @@ in {
     VOLAR_LS = "${pkgs.vue-language-server}/lib/node_modules/@vue/language-server";
   };
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+  # programs.home-manager.enable = true;
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
 }
