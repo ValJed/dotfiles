428a076c2bbe298cb0c2d9bd4daaa211d66f647f
Jed <vjeudy@protonmail.com>
2025-09-22 23:46:14 +0200

    udpates sddm, uses astronaut instead of catuccin (build error)

--- nix/nixosModules/services/displayManager.nix
+++ nix/nixosModules/services/displayManager.nix
@@ -1,4 +1,14 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  sddm-astronaut = pkgs.sddm-astronaut.override {
+    embeddedTheme = "japanese_aesthetic";
+  };
+  # catpuccin-sddm = pkgs.catppuccin-sddm.override {
+  #   flavor = "mocha";
+  #   font = "FiraCode Nerd Font";
+  #   fontSize = "12";
+  #   background = "${../../../assets/wallpaper.png}";
+  # };
+in {
   # Not working
   # services.displayManager.ly = {
   #   enable = true;
@@ -8,19 +18,17 @@
   # };
 
   environment.systemPackages = [
-    (
-      pkgs.catppuccin-sddm.override {
-        flavor = "mocha";
-        font = "FiraCode Nerd Font";
-        fontSize = "12";
-        background = "${../../../assets/wallpaper.png}";
-        loginBackground = true;
-      }
-    )
+    sddm-astronaut
   ];
+
   services.displayManager.sddm = {
     enable = true;
-    theme = "catppuccin-mocha";
+    wayland.enable = true;
+    # "catpuccin-mocha"
+    theme = "sddm-astronaut-theme";
     package = pkgs.kdePackages.sddm;
+    extraPackages = [
+      sddm-astronaut
+    ];
   };
 }
