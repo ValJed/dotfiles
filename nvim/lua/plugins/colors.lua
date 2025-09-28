9b5979a008e8e6526ef97a718271f4f56dccc165
Jed <vjeudy@protonmail.com>
2025-09-28 23:41:02 +0200

    fix coloscheme for nvim

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -6,9 +6,7 @@ return {
 		as = "rose-pine",
 		config = function()
 			local colorscheme = "rose-pine-main"
-			vim.cmd("colorscheme deault")
-
-			local status_ok, _ = pcall(vim, cmd, "colorscheme " .. colorscheme)
+			local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 			if not status_ok then
 				vim.notify("colorscheme " .. " not found!")
 				return
