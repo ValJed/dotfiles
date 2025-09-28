fea74390f094d07f07df8b98087ddae996baa410
Jed <vjeudy@protonmail.com>
2025-09-28 23:49:46 +0200

    reads etc theme file before to select colorscheme

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,3 +1,16 @@
+function ReadFile(filepath)
+	if vim.fn.filereadable(filepath) == 0 then
+		return nil, "File not found or not readable: " .. filepath
+	end
+
+	local lines = vim.fn.readfile(filepath, "", 1) -- Read only 1 line
+	if #lines > 0 then
+		return lines[1]
+	else
+		return "", "File is empty"
+	end
+end
+
 return {
 	"brenoprata10/nvim-highlight-colors",
 	{ "nvim-mini/mini.base16" },
@@ -5,7 +18,16 @@ return {
 		"rose-pine/neovim",
 		as = "rose-pine",
 		config = function()
-			local colorscheme = "rose-pine-main"
+			local dark_theme = "rose-pine-main"
+			local light_theme = "rose-pine-dawn"
+			local cur_theme = ReadFile("/etc/theme")
+			local colorscheme
+			if cur_theme == "light" then
+				colorscheme = light_theme
+			else
+				colorscheme = dark_theme
+			end
+
 			local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 			if not status_ok then
 				vim.notify("colorscheme " .. " not found!")
