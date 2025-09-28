2dcb716992d72271c95cac6d68b69cad6a15e2ac
Jed <vjeudy@protonmail.com>
2025-09-28 15:53:54 +0200

    moves nvim init.lua to nix home manager

--- nix/homeManagerModules/programs/nvim.nix
+++ nix/homeManagerModules/programs/nvim.nix
@@ -5,7 +5,33 @@
     extraLuaPackages = luajitPackages: [
       luajitPackages.inspect
     ];
+    extraLuaConfig = ''
+      require("options")
+      require("keymaps")
+
+      local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+      if not vim.loop.fs_stat(lazypath) then
+      	vim.fn.system({
+      		"git",
+      		"clone",
+      		"--filter=blob:none",
+      		"https://github.com/folke/lazy.nvim.git",
+      		"--branch=stable", -- latest stable release
+      		lazypath,
+      	})
+      end
+
+      vim.opt.rtp:prepend(lazypath)
+
+      require("lazy").setup("plugins", {
+      	dev = {
+      		path = "~/workspace/projects",
+      	},
+      })
+
+      require("autocmd")
+    '';
   };
 
-  stylix.targets.neovim.enable = false;
+  stylix.targets.neovim.enable = true;
 }
