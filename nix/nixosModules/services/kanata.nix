2ce4b675514b0f573a5ae6ff4735f8b8a304016d
Jed <vjeudy@protonmail.com>
2025-10-03 19:23:18 +0200

    updates kanata for laptop

--- nix/nixosModules/services/kanata.nix
+++ nix/nixosModules/services/kanata.nix
@@ -38,7 +38,7 @@
               chord-m 50
             )
             (defalias
-              caps esc
+              caps-word (caps-word-toggle 2000)
               d_shft (tap-hold $tap-time $hold-time d lsft)
               k_shft (tap-hold $tap-time $hold-time k lsft)
               f_meta (tap-hold $tap-time $hold-time f lmeta)
@@ -50,7 +50,7 @@
 
               ;; Layouts
               spc_mod (tap-hold $tap-time $hold-time spc (layer-while-held special))
-              caps_mod (tap-hold $tap-time $hold-time esc (layer-while-held media))
+              media (layer-while-held media)
               game_toggle (switch
                 ((layer game)) (layer-switch base) break
                 () (layer-switch game) break)
@@ -73,35 +73,36 @@
             )
 
             (deflayer base
-              grv   1      2     3    4    5    6    7    8    9     0       -    =    bspc    ;; 14
-              tab   q      w     e    r    t    y    u    i    o     p       \    ]    ret     ;; 14
-              @caps_mod    a     s    d    f    g    h    j    k     l  ;  '       bspc    ;; 13
-              lsft  @z_alt x     c    v    b    n    m    ,    .     @/_alt  rsft    ;; 12
-              lctl  lalt   lmet             @spc_mod            rmet  ralt    rctl    ;; 7
+              @caps-word  1       2     3    4    5    6    7    8       9       0        -      =    bspc    ;; 14
+              tab         q       w     e    r    t    y    u    i       o       p        \      ]    ret     ;; 14
+              esc         a       s     d    f    g    h    j    k       l       ;        '       bspc    ;; 13
+              @media      @z_alt  x     c    v    b    n    m    ,       .       @/_alt   rsft    ;; 12
+              lctl        lalt    lmet       @spc_mod            @media  ralt    rctl     ;; 7
             )
 
             (deflayer special
-              _          f1   f2   f3   f4   f5   f6    f7    f8   f9   f10  f11   f12    _
-              S-prtsc    1    2    3    4    5    6     7     8    9    0    =     S-=    ret
-              caps       S-1  S-2  S-3  S-4  S-5  AG-6  S-9   S-0  S-7  S-8  S-7   bspc
-              C-S-prtsc  S-`  S-=  =    [    ]    AG-`  S-[   S-]  -    S--        rsft
-              _          _    _         @spc_mod         ralt rmet            rctl
+              @caps-word   f1   f2   f3   f4   f5   f6    f7    f8   f9   f10  f11   f12    _
+              grv          1    2    3    4    5    6     7     8    9    0    =     S-=    ret
+              esc          S-1  S-2  S-3  S-4  S-5  AG-6  S-9   S-0  S-7  S-8  S-7   bspc
+              @media       S-`  S-=  =    [    ]    AG-`  S-[   S-]  -    S--        rsft
+              _            _    _         @spc_mod         ralt rmet            rctl
             )
 
+
             (deflayer media
-              _          _          _     bldn       blup    _      _      _      _      _      _       _    _   _
-              _          prtsc      _     mute       pp      _      home   pgdn   pgup   end    C-S-v   _    _   ret
-              _          S-prtsc    C-b   voldwn     volu    _      left   down   up     rght   _       _    _
-              _          C-S-prtsc  _     next prev  next    volu   _      S--    S-7    S-8       rsft
-              _          lmet lalt        @spc_mod           ralt   rmet          rctl
+              _            _          _       bldn       blup    _      _        _        _      _      _       _    _   _
+              _            prtsc      _       mute       pp      _      home     pgdn     pgup   end    C-S-v   _    _   ret
+              esc          S-prtsc    C-b     voldwn     volu    _      left     down     up     rght   _       _    _
+              @media       C-S-prtsc  _       next prev  next    _      C-S-tab  C-tab    _      _      _
+              _            lmet       lalt    @spc_mod           ralt   rmet            rctl
             )
 
             (deflayer game
-              grv   1    2    3    4    5    6    7    8    9    0    -    =    bspc
-              tab   q    w    e    r    t    y    u    i    o    p    [    ]    ret
-              caps  a    s    d    f    g    h    j    k    l    ;    '    \
-              lsft  z    x    c    v    b    n    m    ,    .    /    rsft
-              lctl  lmet lalt           spc            ralt rmet rctl
+              grv     1    2    3    4    5    6    7    8    9    0    -    =    bspc
+              tab     q    w    e    r    t    y    u    i    o    p    [    ]    ret
+              caps    a    s    d    f    g    h    j    k    l    ;    '    \
+              lsft    z    x    c    v    b    n    m    ,    .    /    rsft
+              lctl    lmet lalt           spc            ralt rmet rctl
             )
           '';
         };
