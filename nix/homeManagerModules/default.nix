1129f7650a7b3d13ce1cb892278be768e7572380
Jed <vjeudy@protonmail.com>
2025-10-13 20:42:05 +0200

    pin awscli2 version (new failing)

--- nix/homeManagerModules/default.nix
+++ nix/homeManagerModules/default.nix
@@ -2,6 +2,7 @@
   pkgs,
   lib,
   config,
+  system,
   ...
 }: let
   act-update = import ./scripts/act-update.nix {inherit pkgs;};
@@ -14,6 +15,15 @@
     inherit lib;
   };
   record = import ./scripts/record.nix {inherit pkgs;};
+
+  # Pin version to 2.27 for now
+  awsSrc =
+    import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/e6f23dc08d3624daab7094b701aa3954923c6bbb.tar.gz";
+      sha256 = "sha256-3a7Tha/RwYlzH/v3PJrG7+HjOj4c6YOv2K8sqdGsHVQ=";
+    }) {
+      system = system;
+    };
 in {
   imports = [
     ./programs/bat.nix
@@ -72,7 +82,7 @@ in {
     pkgs.fastfetch
     pkgs.bc
     pkgs.dysk
-    pkgs.awscli2
+    awsSrc.awscli2
     pkgs.ssm-session-manager-plugin
     pkgs.mongosh
     # pkgs.nodePackages.eas-cli Removed for now since breaks at build time
