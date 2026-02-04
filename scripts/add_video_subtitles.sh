76a1889f5c6c81c56410f05e96b0de08ee446751
Jed <vjeudy@protonmail.com>
2026-02-04 18:38:50 +0100

    update add_video_subtitles

--- scripts/add_video_subtitles.sh
+++ scripts/add_video_subtitles.sh
@@ -7,18 +7,68 @@ if [ $# -eq 0 ]; then
 fi
 
 directory_path=$1
-echo $directory_path
+subtitle_path=$2
 
-for path in "$directory_path"/*.mkv; do
+function encode_vid() {
+  vid_path=$1
+  sub_path=$2
+
+  if [ ! -f "$path" ]; then
+    return
+  fi
+
+  # Get file extension
+  ext="${path##*.}"
+  file=$(basename "$path" ".$ext")
+  dir=$(dirname "$path")
+  
+  # Set subtitle codec based on format
+  if [ "$ext" = "mkv" ]; then
+    subtitle_codec="srt"
+  else
+    subtitle_codec="mov_text"
+  fi
+  
+  ffmpeg -i "$path" -f srt -i "$dir/subtitles/$file.srt" \
+  -map 0:0 -map 0:1 -map 1:0 -c:v copy -c:a copy \
+  -c:s "$subtitle_codec" "${dir}/${file}_fr.$ext" &&
+  rm "$path" &&
+  mv "${dir}/${file}_fr.$ext" "$path"
+}
+
+echo $directory_path
+for path in "$directory_path"/*.{mkv,mp4}; do
     if [ -f "$path" ]; then
-      file=$(basename "$path" .mkv)
+      # Get file extension
+      ext="${path##*.}"
+      file=$(basename "$path" ".$ext")
       dir=$(dirname "$path")
-      ffmpeg -i $path -f srt -i $dir/subtitles/$file.srt \
+      
+      # Set subtitle codec based on format
+      if [ "$ext" = "mkv" ]; then
+        subtitle_codec="srt"
+      else
+        subtitle_codec="mov_text"
+      fi
+      
+      ffmpeg -i "$path" -f srt -i "$dir/subtitles/$file.srt" \
       -map 0:0 -map 0:1 -map 1:0 -c:v copy -c:a copy \
-      -c:s srt "${dir}/${file}_fr.mkv" &&
-      rm $path &&
-      mv "${dir}/${file}_fr.mkv" "$path"
+      -c:s "$subtitle_codec" "${dir}/${file}_fr.$ext" &&
+      rm "$path" &&
+      mv "${dir}/${file}_fr.$ext" "$path"
     fi
 done
+
+# for path in "$directory_path"/*.mkv; do
+#     if [ -f "$path" ]; then
+#       file=$(basename "$path" .mkv)
+#       dir=$(dirname "$path")
+#       ffmpeg -i $path -f srt -i $dir/subtitles/$file.srt \
+#       -map 0:0 -map 0:1 -map 1:0 -c:v copy -c:a copy \
+#       -c:s srt "${dir}/${file}_fr.mkv" &&
+#       rm $path &&
+#       mv "${dir}/${file}_fr.mkv" "$path"
+#     fi
+# done
 
 echo "All conversions completed."
