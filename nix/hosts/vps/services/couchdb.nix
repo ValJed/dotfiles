3d1e326d9105e185dbb210dc8dbe5538ae4cae6e
Jed <vjeudy@protonmail.com>
2026-09-08 23:13:45 +0200

    nixos couchdb update

--- nix/hosts/vps/services/couchdb.nix
+++ nix/hosts/vps/services/couchdb.nix
@@ -9,16 +9,17 @@
     bindAddress = "127.0.0.1";
     port = 5984;
 
-    extraConfig = ''
-      [couchdb]
-      single_node=true
-
-      [chttpd]
-      require_valid_user = true
-    '';
+    extraConfig = {
+      couchdb.single_node = true;
+      chttpd.require_valid_user = true;
+    };
 
     extraConfigFiles = [config.sops.secrets.couchdb-admins-ini.path];
   };
+
+  environment.systemPackages = with pkgs; [
+    couchdb-dump
+  ];
 
   sops.secrets.couchdb-admins-ini = {
     owner = config.services.couchdb.user;
